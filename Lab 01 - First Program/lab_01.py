6d0b4409acfce78f6dec3fd4cc3bca0079b89714
Atalay <atalayinthesky@gmail.com>
2024-11-28 15:27:03 +0300

    first lab completed. print function and escape codes was the focus

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -5,7 +5,7 @@ print("you gotta know that")
 print("this is a waste of time.")
 print("\t tab indented")
 
-print("escape codes are easy. if you want to include a double quote, put a backslash before it. \"this is in quotes.\"")
+print("escape codes are easy.\nif you want to include a double quote, put a backslash before it. \"this is in quotes.\"")
 print("also, to go to next line, use \\n. how did i print \ before n here, you may ask. put a backslash before that too")
 print("""3 double quotes at the beginning and the end
 it will let you write a big chunk of text without using \\n""")
