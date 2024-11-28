817da288c13d5d6d73d3a4e10d765c38778d9730
Atalay <atalayinthesky@gmail.com>
2024-11-28 15:23:45 +0300

    first lab completed. bunch of print statements.

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -3,3 +3,10 @@ print("no one will ever read this")
 print("but if you do")
 print("you gotta know that")
 print("this is a waste of time.")
+print("\t tab indented")
+
+print("escape codes are easy. if you want to include a double quote, put a backslash before it. \"this is in quotes.\"")
+print("also, to go to next line, use \\n. how did i print \ before n here, you may ask. put a backslash before that too")
+print("""3 double quotes at the beginning and the end
+it will let you write a big chunk of text without using \\n""")
+print("cant do it with normal print. tested")
