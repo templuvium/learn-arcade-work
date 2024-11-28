908adc096f64acd8a3db1c08a59ddefa32976aa8
Atalay <atalayinthesky@gmail.com>
2024-11-28 13:53:36 +0300

    chapter 4 -print function- finished, commit before first lab

---
+++ Testing/test.py
@@ -0,0 +1,6 @@
+print("Hi, this is a 'double' quote mark: \"Double quotes\". Also, this is a next line: \n underground... ")
+print("This file is stored in C:\\new folder")
+print(""""This text is so big
+that i have to use 
+3 double quotes.
+crazy!""")
